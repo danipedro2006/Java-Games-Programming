6b028ebe6298f73da251a7bcb5d1c9303148b2c5
daniel pirvu <danipedro2006@yahoo.com>
2021-06-24 18:15:45 +0300

    JFrame window ex

---
+++ Java Swing Timer/src/Game.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Game {
+
+	public static void main(String[] args) {
+		JFrame frame=new JFrame("Mini Tenis");
+		frame.setSize(300,300);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
