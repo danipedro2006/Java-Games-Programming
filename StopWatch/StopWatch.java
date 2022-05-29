28168d1431643bb659d2693967f298de0b85f036
Daniel Pirvu <danipedro2006@yahoo.com>
2022-05-29 11:08:10 +0300

    new features add

--- StopWatch/StopWatch.java
+++ StopWatch/StopWatch.java
@@ -2,14 +2,21 @@ package SafeCode;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class StopWatch extends JFrame{
 	
 	JButton startButton=new JButton();
 	JButton stopButton=new JButton();
+	JLabel startLabel=new JLabel();
+	JTextField startTextField=new JTextField();
+	JTextField stopTextField=new JTextField();
 	
 	public StopWatch() {
 		
@@ -28,7 +35,46 @@ public class StopWatch extends JFrame{
 		gridConstraints.gridx=0;
 		gridConstraints.gridy=1;
 		getContentPane().add(stopButton, gridConstraints);
+		
+		startLabel.setText(" Start time ");
+		gridConstraints.gridx=1;
+		gridConstraints.gridy=0;
+		getContentPane().add(startLabel, gridConstraints);
+		
+		
+		
+		startTextField.setText("");
+		gridConstraints.gridx=2;
+		gridConstraints.gridy=0;
+		startTextField.setColumns(15);
+		getContentPane().add(startTextField, gridConstraints);
+		
+		stopTextField.setText("");
+		gridConstraints.gridx=2;
+		gridConstraints.gridy=1;
+		stopTextField.setColumns(15);
+		getContentPane().add(stopTextField, gridConstraints);
+		
+		startButton.addActionListener(new ActionListener() {
+			private long startTime;
+
+			public void actionPerformed(ActionEvent e) {
+				startButtonActionPerformed(e);
+			}
+
+			private void startButtonActionPerformed(ActionEvent e) {
+				startTime=System.currentTimeMillis();
+				startTextField.setText(String.valueOf(startTime));
+				
+			}
+
+			
+		});
 	}
+	
+	
+	
+	
 	public static void main(String[] args) {
 		new StopWatch().show();
 		
